124843343c687d8fb916e6a73c4306f3a59d0632
josegbel <jose.gbel@gmail.com>
2024-01-11 14:57:55 +0000

    chore: added navigation

--- App.tsx
+++ App.tsx
@@ -1,80 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
-import * as SplashScreen from 'expo-splash-screen'
-import {useCallback, useEffect, useState} from "react";
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler'
 import * as Font from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+import {StatusBar} from 'expo-status-bar';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {useCallback, useEffect, useState} from "react";
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
 
-SplashScreen.preventAutoHideAsync();
+import ChatListScreen from './screens/ChatListScreen'
+import ChatSettingsScreen from "./screens/ChatSettingsScreen";
+
+SplashScreen.preventAutoHideAsync()
+
+const Stack = createStackNavigator()
 
 export default function App() {
-  
-  const [appIsLoaded, setAppIsLoaded] = useState(false);
-  
-  useEffect(() => {
-    // Load fonts
-    const prepare = async () => {
-      try {
-        await Font.loadAsync({
-          "black": require("./assets/fonts//Roboto-Black.ttf"),
-          "blackItalic": require("./assets/fonts/Roboto-BlackItalic.ttf"),
-          "bold": require("./assets/fonts/Roboto-Bold.ttf"),
-          "boldItalic": require("./assets/fonts/Roboto-BoldItalic.ttf"),
-          "italic": require("./assets/fonts/Roboto-Italic.ttf"),
-          "light": require("./assets/fonts/Roboto-Light.ttf"),
-          "lightItalic": require("./assets/fonts/Roboto-LightItalic.ttf"),
-          "medium": require("./assets/fonts/Roboto-Medium.ttf"),
-          "mediumItalic": require("./assets/fonts/Roboto-MediumItalic.ttf"),
-          "regular": require("./assets/fonts/Roboto-Regular.ttf"),
-          "thin": require("./assets/fonts/Roboto-Thin.ttf"),
-          "thinItalic": require("./assets/fonts/Roboto-ThinItalic.ttf"),
-        })
-      }
-      catch (error) {
-        console.log(error)
-      }
-      finally {
-          setAppIsLoaded(true)
-      }
+
+    const [appIsLoaded, setAppIsLoaded] = useState(false);
+
+    useEffect(() => {
+        // Load fonts
+        const prepare = async () => {
+            try {
+                await Font.loadAsync({
+                    "black": require("./assets/fonts//Roboto-Black.ttf"),
+                    "blackItalic": require("./assets/fonts/Roboto-BlackItalic.ttf"),
+                    "bold": require("./assets/fonts/Roboto-Bold.ttf"),
+                    "boldItalic": require("./assets/fonts/Roboto-BoldItalic.ttf"),
+                    "italic": require("./assets/fonts/Roboto-Italic.ttf"),
+                    "light": require("./assets/fonts/Roboto-Light.ttf"),
+                    "lightItalic": require("./assets/fonts/Roboto-LightItalic.ttf"),
+                    "medium": require("./assets/fonts/Roboto-Medium.ttf"),
+                    "mediumItalic": require("./assets/fonts/Roboto-MediumItalic.ttf"),
+                    "regular": require("./assets/fonts/Roboto-Regular.ttf"),
+                    "thin": require("./assets/fonts/Roboto-Thin.ttf"),
+                    "thinItalic": require("./assets/fonts/Roboto-ThinItalic.ttf"),
+                })
+            } catch (error) {
+                console.log(error)
+            } finally {
+                setAppIsLoaded(true)
+            }
+        }
+        prepare()
+    })
+
+    const onLayout = useCallback(async () => {
+        if (appIsLoaded) {
+            await SplashScreen.hideAsync()
+        }
+    }, [appIsLoaded])
+
+    if (!appIsLoaded) {
+        return null
     }
-    prepare()
-  })
-  
-  const onLayout = useCallback(async () => {
-    if(appIsLoaded){
-      await SplashScreen.hideAsync()
-    }
-  }, [appIsLoaded])
-  
-  if(!appIsLoaded){
-    return null
-  }
-  
-  return (
-    <SafeAreaProvider
-      style={styles.container}
-      onLayout={onLayout}>
-      <SafeAreaView>
-      <View style={styles.container}>
-        <Text style={styles.label}>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
-      </SafeAreaView>
-    </SafeAreaProvider>
-  );
+
+    return (
+        <SafeAreaProvider
+            style={styles.container}
+            onLayout={onLayout}>
+            <NavigationContainer>
+
+                <Stack.Navigator>
+                    <Stack.Screen name="Home" component={ChatListScreen}/>
+                    <Stack.Screen name="ChatSettings" component={ChatSettingsScreen}/>
+                </Stack.Navigator>
+
+            </NavigationContainer>
+        </SafeAreaProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  label: {
-    color: 'black',
-    fontSize: 18,
-    fontFamily: "regular"
-  }
-  
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    label: {
+        color: 'black',
+        fontSize: 18,
+        fontFamily: "regular"
+    }
 });
